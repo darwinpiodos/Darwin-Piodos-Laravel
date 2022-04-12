128787f241f5f6445306e94b92913a2444cf473f
darwinpiodos <darwindivinopiodos21@gmail.com>
2022-04-12 08:03:10 +0800

    Drop down toogle , Logout function

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
 
 
 Route::post('register',[UserController::class,'register']); 
+Route::post('login',[UserController::class,'login']); 
+
+Route::post('addproduct',[ProductController::class,'addProduct']); 
+
+
 
 
 
